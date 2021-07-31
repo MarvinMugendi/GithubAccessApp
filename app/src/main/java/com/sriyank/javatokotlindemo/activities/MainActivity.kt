ca4f4c5f0d1a49bd5425f3a134df88219cfd3e36
MarvinMugendi <marvedie254@gmail.com>
2021-07-31 03:46:05 -0400

    MainActivity, now in Kotlin

---
+++ app/src/main/java/com/sriyank/javatokotlindemo/activities/MainActivity.kt
@@ -0,0 +1,117 @@
+package com.sriyank.javatokotlindemo.activities
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.TextInputLayout
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.EditText
+import com.sriyank.javatokotlindemo.R
+import com.sriyank.javatokotlindemo.app.Constants
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    /** Java equivalent would be
+     *
+     *
+     * Private final String TAG = MainActivity.class.getSimpleName();*/
+    companion object {
+
+        private val TAG: String = MainActivity::class.java.simpleName
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        /**Accessing the TAG in MainActivity.kt
+         * To access in any other class,
+         * Make it public
+
+        MainActivity.TAG
+
+         */
+
+    }
+
+    /** Save app username in Shared Preferences*/
+    fun saveName(view: View) {
+
+
+    }
+
+    /**Search repositories on Github after passing data to DisplayActivity*/
+    fun listRepositories(view: View) {
+
+        /**Add validation rule */
+
+        if (isNotEmpty(etRepoName, inputLayoutRepoName)) {
+            /**Fetch specific EditText using KotlinAndroid Extensions*/
+            val queryRepo = etRepoName.text.toString()
+            val repoLanguage = etLanguage.text.toString()
+
+            /**Passing Intents using the Constance Java Class to DisplayActivity*/
+            val intent = Intent(this@MainActivity, DisplayActivity::class.java)
+            intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_REPO)
+            intent.putExtra(Constants.KEY_REPO_SEARCH, queryRepo)
+            intent.putExtra(Constants.KEY_LANGUAGE, repoLanguage)
+            startActivity(intent)
+        }
+
+
+
+    }
+
+
+    /**Search repositories of a particular Github user after passing data to DisplayActivity*/
+    fun listUserRepositories(view: View) {
+
+        if(isNotEmpty(etGithubUser, inputLayoutGithubUser)) {
+
+
+            val githubUser = etGithubUser.text.toString()
+
+            //Code to show Kotlin Reflection classes.
+            val cls: Class<DisplayActivity> = DisplayActivity::class.java
+
+            val intent = Intent(this@MainActivity, cls)
+
+            //cls = DisplayActivity::class.java
+            /**Specify query type*/
+            intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_USER)
+            /**Pass the githubUser intent*/
+            intent.putExtra(Constants.KEY_GITHUB_USER, githubUser)
+            startActivity(intent)
+        }
+    }
+
+    /** Validation Rule
+     * This function checks to see if the editTexts are blank
+     * and returns an error to the user.
+     */
+
+    fun isNotEmpty(editText: EditText, textInputLayout: TextInputLayout) : Boolean {
+
+        if (editText.toString().isEmpty()){
+            //set validation error
+            textInputLayout.error = "Cannot be blank !"
+            return false
+        }
+        else{
+            textInputLayout.isErrorEnabled = false
+            return true
+        }
+    }
+
+
+
+
+
+
+
+}
